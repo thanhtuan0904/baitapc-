b479bdb9c4445b2d449ca3b363d0fa9c3c3a50eb
thanhtuan0904 <iamtuan090404@gmail.com>
2025-03-31 02:04:16 -0700

    update

---
+++ bai1/bai1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Student
+{
+    private int rollno;
+    private string name;
+    private static string college = "Ptit";
+
+    public Student(int rollno, string name)
+    {
+        this.rollno = rollno;
+        this.name = name;
+    }
+    // phuong thuc change de thay doi gia tri tinh cua bien
+    public static void Change()
+    {
+        college = "Dai Hoc Kien Truc";
+    }
+    public void Display()
+    {
+        Console.WriteLine(rollno+" "+name+" "+college);
+    }
+    public static void Main(string[] args)
+    {
+        Student s1 = new Student(1,"tuan") ;
+        
+        s1.Display();
+        Student s2 = new Student(2, "thanh");
+        s2.Display();
+    }
+  }
